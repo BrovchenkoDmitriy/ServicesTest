99a4ddbc5c28e3adfff4bcac93a3961f9e903c6f
Morak <d.a.brovchenko@gmail.com>
2022-06-10 15:18:03 +0900

    CombinationJobSchedulerAndIntentService

--- app/src/main/java/com/example/servicestest/MainActivity.kt
+++ app/src/main/java/com/example/servicestest/MainActivity.kt
@@ -51,6 +51,8 @@ class MainActivity : AppCompatActivity() {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
                 val intent = MyJobService.newIntent(page++)
                 jobScheduler.enqueue(jobInfo, JobWorkItem(intent))
+            } else{ //если SDK меньше API 26, то используем всместо JobScheduler IntentService
+             startService(MyIntentService2.newIntent(this, page++))
             }
         }
     }
---
+++ app/src/main/java/com/example/servicestest/MyIntentService2.kt
@@ -0,0 +1,59 @@
+package com.example.servicestest
+
+import android.app.IntentService
+import android.content.Context
+import android.content.Intent
+import android.os.IBinder
+import android.util.Log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancel
+
+class MyIntentService2 : IntentService(NAME) {
+
+    private val scope = CoroutineScope(Dispatchers.Main)
+
+    override fun onCreate() {
+        super.onCreate()
+        log("onCreate")
+        setIntentRedelivery(true)
+
+    }
+
+    override fun onHandleIntent(intent: Intent?) {
+        log("onHandleIntent")
+        val page = intent?.getIntExtra(PAGE, 0)?:0
+        for (i in 0..10) {
+            Thread.sleep(1000)
+            log("Timer $i $page")
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        scope.cancel()
+        log("onDestroy")
+    }
+
+
+    override fun onBind(intent: Intent?): IBinder? {
+        TODO("Not yet implemented")
+    }
+
+    companion object {
+        private const val NAME = "MyIntentService2"
+        private const val PAGE = "page"
+        fun newIntent(context: Context, page: Int): Intent {
+            return Intent(context, MyIntentService2::class.java).apply {
+                putExtra(PAGE, page)
+            }
+
+        }
+    }
+
+    private fun log(message: String) {
+        Log.d("SERVICE_TAG", "MyIntentService2 $message")
+
+    }
+
+}
