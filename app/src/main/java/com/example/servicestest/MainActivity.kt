29bcf69523e324427b2efb2068e2b65342d870c7
BrovchenkoDmitriy <99586855+BrovchenkoDmitriy@users.noreply.github.com>
2022-11-14 17:24:24 +0900

    AlarmManager

--- app/src/main/java/com/example/servicestest/MainActivity.kt
+++ app/src/main/java/com/example/servicestest/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.servicestest
 
+import android.app.AlarmManager
+import android.app.PendingIntent
 import android.app.job.JobInfo
 import android.app.job.JobScheduler
 import android.app.job.JobWorkItem
@@ -13,6 +15,7 @@ import androidx.core.content.ContextCompat
 import androidx.work.ExistingWorkPolicy
 import androidx.work.WorkManager
 import com.example.servicestest.databinding.ActivityMainBinding
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -97,6 +100,21 @@ class MainActivity : AppCompatActivity() {
                 MyWorker.makeRequest(page++) // через OneTimeWorkRequest передаем page и ограничения
             )
         }
+
+        binding.alarmManager.setOnClickListener {
+            val alarmManager = getSystemService(ALARM_SERVICE) as AlarmManager
+            val calendar = Calendar.getInstance()
+            calendar.add(Calendar.SECOND, 30)
+
+            val intent = AlarmReceiver.newIntent(this)
+            val pendingIntent = PendingIntent.getBroadcast(this, 100, intent, 0)
+
+            alarmManager.setExact(
+                AlarmManager.RTC_WAKEUP,
+                calendar.timeInMillis,
+                pendingIntent
+            )
+        }
     }
 
     override fun onStart() {
@@ -105,7 +123,7 @@ class MainActivity : AppCompatActivity() {
             MyForegroundService.newIntent(this),
             serviceConnection,
             0
-            )
+        )
     }
 
     override fun onStop() {
